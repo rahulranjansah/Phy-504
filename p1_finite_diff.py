da5280b2c491799f3c9cfe77c8d6bee92190c50f
Rahul Ranjan Sah <rahulranjann45@gmail.com>
2025-02-05 19:08:09 -0500

    week 1, psets

---
+++ p1_finite_diff.py
@@ -0,0 +1,113 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def f(x):
+
+    """
+    Function to compute the value of e^(-x^2).
+
+    Parameters:
+    x (float): Input value.
+
+    Returns:
+    float: Computed value of e^(-x^2).
+    """
+
+    return math.exp((-x ** 2))
+
+def f_first_finite_diff(f, x, h):
+    """
+    Computes the first finite difference approximation of the derivative of f at x.
+
+    Parameters:
+    f (function): Function whose derivative is to be approximated.
+    x (float): Point at which the derivative is to be approximated.
+    h (float): Step size.
+
+    Returns:
+    float: First finite difference approximation of the derivative of f at x.
+    """
+    return ((f(x+h) - f(x-h)) / (2 * h))
+
+def f_second_finite_diff(f, x, h):
+    """
+    Computes the second finite difference approximation of the second derivative of f at x.
+
+    Parameters:
+    f (function): Function whose second derivative is to be approximated.
+    x (float): Point at which the second derivative is to be approximated.
+    h (float): Step size.
+
+    Returns:
+    float: Second finite difference approximation of the second derivative of f at x.
+    """
+    return ((f(x+h) - (2 * f(x)) + f(x-h)) / (h ** 2))
+
+def f_prime_analytical(x):
+    """
+    Computes the analytical first derivative of f at x.
+
+    Parameters:
+    x (float): Point at which the first derivative is to be computed.
+
+    Returns:
+    float: Analytical first derivative of f at x.
+    """
+    return (-2*x*math.exp(-x ** 2))
+
+def f_double_prime_analytical(x):
+    """
+    Computes the analytical second derivative of f at x.
+
+    Parameters:
+    x (float): Point at which the second derivative is to be computed.
+
+    Returns:
+    float: Analytical second derivative of f at x.
+    """
+    return (4*x*math.exp(-x ** 2))-(2*math.exp(-x ** 2))
+
+x = 6
+
+# given stability region (10^-4 to 10^-8)
+h = 10 ** -5
+
+print(f"First Finite Difference: {f_first_finite_diff(f, x, h)}")
+print(f"Second Finite Difference: {f_second_finite_diff(f, x, h)}")
+
+print(f"First Prime Analytical: {f_prime_analytical(x)}")
+print(f"Second Prime Analytical: {f_double_prime_analytical(x)}")
+
+
+h_values = np.logspace(-15, 0, 100)
+errors_first_derivative = []
+errors_second_derivative = []
+
+for h in h_values:
+
+    # derivatives
+    f_prime_num = f_first_finite_diff(f, x, h)
+    f_double_prime_num = f_second_finite_diff(f, x, h)
+    f_prime_ana = f_prime_analytical(x)
+    f_double_prime_ana = f_double_prime_analytical(x)
+
+    # abs errors
+    error_first = np.abs(f_prime_num - f_prime_ana)
+    error_second = np.abs(f_double_prime_num - f_double_prime_ana)
+
+    # Store errors
+    errors_first_derivative.append(error_first)
+    errors_second_derivative.append(error_second)
+
+# Plot the results
+plt.figure(figsize=(10, 6))
+plt.loglog(h_values, errors_first_derivative, label="First Derivative Error", marker=".")
+plt.loglog(h_values, errors_second_derivative, label="Second Derivative Error", marker="x")
+plt.axvline(x=1e-8, color="red", linestyle="--", label="Optimal h (approx)")
+plt.xlabel("Step Size (h)")
+plt.ylabel("Error")
+plt.title("Error in Numerical Derivatives vs. Step Size (h)")
+plt.legend()
+plt.grid(True, which="both", ls="--")
+plt.show()
---
+++ p2_harmonic.py
@@ -0,0 +1,80 @@
+# imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+omega_i = 1.5 * 10 ** 16 # heartz
+omega = [0.5, 1.0, 1.5]
+
+t_0 = 5 * 10 ** (-15) # seconds
+t_max = 100 * 10 ** (-15) # seconds
+q_electron = - 1.6 * 10 ** (-19) # charge
+m_electron = 9.1 * 10 ** (-31) # kg
+gamma = 0.01 * omega_i
+
+# E vs t graph
+
+delta_t = 1.0e-17
+num_steps = int(t_0 / delta_t) * 100
+
+# print(num_steps)
+
+t = np.linspace(-t_0*10, t_max, num_steps)
+E_0 = [1.0, 1.0 * 10**10] # N/C
+
+def dampend_oscillations(omega: float, E_0: float) -> float:
+
+    """
+    Simulates damped oscillations of a driven harmonic oscillator.
+
+    Parameters:
+    omega (float): Angular frequency of the oscillator.
+    E_0 (float): Amplitude of the electric field.
+
+    Returns:
+    np.ndarray: Array of positions x over time.
+    """
+    x = np.zeros(num_steps)
+    v = np.zeros(num_steps)
+
+    x[0] = 0
+    v[0] = 0
+
+    # compute for given num steps
+    for i in range(1, num_steps-1):
+
+        # Electric field
+        E = E_0 * np.exp(-(t[i] / t_0) ** 2) * np.cos(omega * omega_i * t[i])
+
+        # ODE
+        x[i+1] = (2 * q_electron *  E * delta_t ** 2) / (m_electron * (2 + (gamma * delta_t))) + \
+                    ((4 * x[i]) - (2 * (x[i - 1])) + (x[i - 1] * delta_t) - (2 * x[i] * delta_t ** 2)) / \
+                        (2 + gamma * delta_t)
+
+    return x
+
+plots = []
+
+# plots all the plots with nested omega vals
+for E0_val in E_0:
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    for omega_val in omega:
+        x = dampend_oscillations(omega_val, E0_val)
+
+        ax.plot(t, x,
+                alpha=0.5,
+                label=f"$\\omega = {omega_val} \\, \\omega_i$")
+
+    ax.set_xlabel("Time $t$ (s)")
+    ax.set_ylabel("Position $x(t)$")
+    ax.set_title(f"Damped-Driven Harmonic Oscillator (E0 = {E0_val} N/C)")
+    ax.legend()
+    ax.grid(ls="dashed", alpha=0.4)
+
+    plots.append(fig)
+
+for fig in plots:
+    plt.show()
+
+
